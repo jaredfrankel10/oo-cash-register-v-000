0f4569f10338b491ce78af842db9b9ba9563e368
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 21:14:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,22 +1,22 @@
 require 'pry'
-  
+
  class CashRegister
    attr_reader :discount, :items
    attr_accessor :total
- 
+
    def initialize(discount = nil)
      @total = 0
      @discount = discount
      @items = []
    end
- 
+
    def add_item(name, cost, quantity = 1)
      @previous_items = @items
      @previous_total = @total
      quantity.times { |_i| @items << name }
      @total += cost * quantity
    end
- 
+
    def apply_discount
      if @discount
        @total = (@total.to_f * (100.0 - @discount.to_f) / 100.0).to_i
@@ -25,10 +25,10 @@ require 'pry'
        "There is no discount to apply."
      end
    end
- 
+
    def void_last_transaction
      @items = @previous_items
      @total = @previous_total
    end
- 
+
  end
